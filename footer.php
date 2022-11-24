32e9f6fe8891620c1b2d042bf48ef3e1b54b4296
Ramon Saldanha <ramonsaldanhaa@gmail.com>
2022-11-24 12:07:11 -0300

    footer add .

--- footer.php
+++ footer.php
@@ -15,37 +15,6 @@
 		<?php endif ?>
 		
 		
-		<div class="wrapper py-3" id="wrapper-footer-colophon">
-			<div class="container-fluid">
-		
-				<div class="row">
-		
-					<div class="col text-center">
-		
-						<footer class="site-footer" id="colophon">
-		
-							<div class="site-info">
-		
-								<?php picostrap_site_info(); ?>
-		
-							</div><!-- .site-info -->
-		
-						</footer><!-- #colophon -->
-		
-					</div><!--col end -->
-		
-				</div><!-- row end -->
-		
-			</div><!-- container end -->
-		
-		</div><!-- wrapper end -->
-		
-	<?php 
-	} //END ELSE CASE ?>
-
-
-	<?php // wp_footer(); ?>
-	
 	</body>
 </html>
 
