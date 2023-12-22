f4046f06476e41a34125d4489335e2d3213e473a
Ramon Saldanha <ramonsaldanhaa@gmail.com>
2023-12-22 16:20:36 -0300

    Add Google tag (gtag.js) for tracking website analytics

--- header.php
+++ header.php
@@ -28,6 +28,18 @@ defined('ABSPATH') || exit;
 
     gtag('config', 'AW-934345590');
   </script>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-YF76FDGBJ9"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-YF76FDGBJ9');
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
